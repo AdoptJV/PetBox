891b6942c80adc5a30aeee9b50e80247df9eb85e
Borbolino <jpbarioni@usp.br>
2025-07-02 23:13:07 -0300

    Consertado bug na Landing Page

--- react-frontend/src/components/pages/LandingPageFiles/Components/LandingLogo.jsx
+++ react-frontend/src/components/pages/LandingPageFiles/Components/LandingLogo.jsx
@@ -5,7 +5,8 @@ function LandingLogo() {
     style={{ filter: "drop-shadow(0 2px 4px rgba(0,0,0,0.2))" +
             "drop-shadow(0 4px 8px rgba(0,0,0,0.2)) " +
             "drop-shadow(0 8px 16px rgba(0,0,0,0.2))",
-        transform: "translateZ(0)" }}
+        transform: "translateZ(0)",
+        maxWidth: "100%"}}
     />;
 }
 
